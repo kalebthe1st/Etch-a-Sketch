18b0fad2c94ac4da9a803de97007467858efc533
kalebthe1st <88403660+kalebthe1st@users.noreply.github.com>
2023-10-09 12:05:01 -0700

    added the div for loop

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
-const box = document.querySelectorAll('.col');
-
-box.forEach((col) => {
-    col.addEventListener("dragover", () => {
-        col.style.backgroundColor = 'black';
-
-        col.addEventListener("contextmenu", () => {
-            col.style.backgroundColor = '';
-        })
+for (let n = 0; n < 10; n++) {
     
+    const cont = document.querySelector('.container');
+    const rowDiv = document.createElement('div');
+    rowDiv.classList.add('row');
+    cont.appendChild(rowDiv)
+    for (let n = 0; n < 10; n++) {
+    
+    
+    
+        const colDiv = document.createElement('div');
+        colDiv.classList.add('col');
+        rowDiv.appendChild(colDiv);
+        
+    
+        // Create a new div element with the class row
+    }
+}
+
+const box = document.querySelectorAll('.col');
+box.forEach((col) => {
+    col.addEventListener("mouseover", () => {
+        col.style.backgroundColor = 'black';
     })
-   
+
 
 })
-
 
