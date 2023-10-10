39ce453721ecdfb35bb3ae9954eee82f85cc2b19
kalebthe1st <88403660+kalebthe1st@users.noreply.github.com>
2023-10-10 07:00:56 -0700

    added buttons fixed the style and some java

--- main.js
+++ main.js
@@ -1,28 +1,54 @@
-for (let n = 0; n < 10; n++) {
-    
-    const cont = document.querySelector('.container');
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add('row');
-    cont.appendChild(rowDiv)
-    for (let n = 0; n < 10; n++) {
-    
-    
-    
-        const colDiv = document.createElement('div');
-        colDiv.classList.add('col');
-        rowDiv.appendChild(colDiv);
-        
-    
-        // Create a new div element with the class row
+
+setSize(16);
+function setSize(input) {
+    for (let n = 0; n < input; n++) {
+
+        const cont = document.querySelector('.container');
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
+        cont.appendChild(rowDiv)
+        for (let n = 0; n < input; n++) {
+
+
+
+            const colDiv = document.createElement('div');
+            colDiv.classList.add('col');
+            rowDiv.appendChild(colDiv);
+
+
+            // Create a new div element with the class row
+        }
     }
 }
 
 const box = document.querySelectorAll('.col');
+
 box.forEach((col) => {
     col.addEventListener("mouseover", () => {
+
+
         col.style.backgroundColor = 'black';
     })
 
 
 })
 
+const btn = document.querySelectorAll('button');
+let btnClor = ""
+
+
+// we use the .forEach method to iterate through each button
+btn.forEach((button) => {
+
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+        // if (button.id == "black") { 
+        //     let btnClor = button.id;
+        // }
+        // else if(button.id == "eraser"){
+        //     col.style.backgroundColor = '#fff';
+        // }
+        btnClor = button.id;
+
+    })
+})
